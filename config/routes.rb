66a71706d37e847315b6d9c350f68a2c64a79461
smr-fpp <105736014+smr-fpp@users.noreply.github.com>
2022-06-15 16:22:02 +0000

    [making]Bookers userのshowページから

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  root to: 'homes#top'
+  get '/home/about' => 'homes#about', as: "about"
+  get 'users/:id/edit' => 'users#edit', as: 'edit_user'
+  get 'users/:id' => 'users#show', as: 'user_show'
+  
+  
+  resources :users, only:[:show, :update, :index, :edit]
+  resources :books, only:[:create, :index, :show, :edit, :destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
