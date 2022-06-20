46df4441a2dd0db050d447bfc101c02970bacf9b
smr-fpp <105736014+smr-fpp@users.noreply.github.com>
2022-06-20 12:28:05 +0000

    [maiking]Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,39 +3,56 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
-    
+    @user = current_user
   end
   
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
+    
+    if @book.save
+      @user = current_user
+      redirect_to book_path(@book), notice: 'You have created book successfully.'
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
   end
   
   def show
     @book = Book.find(params[:id])
-    
+    @user = @book.user
   end
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
+    redirect_to books_path
   end
   
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'You have updated book successfully.'
+    else
+      render :edit
+    end
   end
   
   private
+  
   def book_params
     params.require(:book).permit(:title, :body)
   end
+  
 end
